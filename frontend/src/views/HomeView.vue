fb2edf41dd43ae53922c71d56c3b388893eca39a
borsTiHD <bastian.j@gmx.net>
2025-01-11 10:30:26 +0100

    feat: add greeting functionality in HomeView; update TypeScript paths and Vite config

--- frontend/src/views/HomeView.vue
+++ frontend/src/views/HomeView.vue
@@ -1,9 +1,21 @@
 <script setup lang="ts">
-import TheWelcome from '../components/TheWelcome.vue'
+import { ref } from 'vue'
+import TheWelcome from '@/components/TheWelcome.vue'
+import { Greet } from 'wailsjs/go/backend/App'
+
+const input = ref('')
+
+const onClick = async () => {
+  const greeting = await Greet(input.value)
+  alert(greeting)
+}
 </script>
 
 <template>
   <main>
+    <input v-model="input" type="text" placeholder="Enter your name" />
+    <button @click="onClick">Greetings</button>
+
     <TheWelcome />
   </main>
 </template>
--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -12,7 +12,8 @@ export default defineConfig({
   ],
   resolve: {
     alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url))
+      '@': fileURLToPath(new URL('./src', import.meta.url)),
+      'wailsjs/go': fileURLToPath(new URL('./wailsjs/go', import.meta.url)),
     },
   },
 })
