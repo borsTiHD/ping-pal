30d1397c551cd5e1624720a05083355668983a3f
borsTiHD <bastian.j@gmx.net>
2025-01-11 12:37:54 +0100

    feat: replace greeting functionality with a new Test component in Index.vue

---
+++ frontend/src/components/Test.vue
@@ -0,0 +1,31 @@
+<script setup lang="ts">
+import { ref } from 'vue'
+import { Greet } from 'wailsjs/go/backend/App'
+
+const input = ref('')
+const response = ref('')
+
+async function onClick() {
+  const greeting = await Greet(input.value)
+  response.value = greeting
+}
+</script>
+
+<template>
+  <div class="flex flex-col items-center p-4 gap-4">
+    <h1 className="text-3xl font-bold underline text-blue-500">
+      Hello world!
+    </h1>
+
+    <div class="flex items-center gap-1">
+      <InputText v-model="input" name="username" type="text" placeholder="Enter your name" @keyup.enter="onClick" />
+      <Button label="Submit" @click="onClick" />
+    </div>
+
+    <div class="flex flex-col items-center p-4">
+      <p class="text-lg font-semibold text-green-500">
+        {{ response }}
+      </p>
+    </div>
+  </div>
+</template>
--- frontend/src/views/Index.vue
+++ frontend/src/views/Index.vue
@@ -1,35 +1,9 @@
 <script setup lang="ts">
-import { ref } from 'vue'
-import { Greet } from 'wailsjs/go/backend/App'
-
-const input = ref('')
-const response = ref('')
-
-async function onClick() {
-  const greeting = await Greet(input.value)
-  response.value = greeting
-}
+import Test from '@/components/Test.vue'
 </script>
 
 <template>
   <main>
-    <div class="flex flex-col items-center p-4">
-      <h1 className="text-3xl font-bold underline text-red-500">
-        Hello world!
-      </h1>
-      <input v-model="input" type="text" placeholder="Enter your name">
-      <button class="p-4 dark:bg-red-400" @click="onClick">
-        Greetings
-      </button>
-    </div>
-
-    <div class="flex flex-col items-center p-4">
-      <h2 class="text-2xl font-bold underline text-blue-500">
-        Response
-      </h2>
-      <p class="text-lg font-semibold text-green-500">
-        {{ response }}
-      </p>
-    </div>
+    <Test />
   </main>
 </template>
