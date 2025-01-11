b582185a69c8059ed21d46c1841945370f77e3b8
borsTiHD <bastian.j@gmx.net>
2025-01-11 15:45:31 +0100

    feat: enhance Test component with loading state and configuration display

--- frontend/src/components/Test.vue
+++ frontend/src/components/Test.vue
@@ -1,6 +1,12 @@
 <script setup lang="ts">
-import { ref } from 'vue'
+import { useAppNameQuery, useConfigQuery } from '@/queries/config'
+import { computed, ref } from 'vue'
 import { Greet } from 'wailsjs/go/main/App'
+
+const { data: config, isFetching: configIsFetching } = useConfigQuery()
+const { data: appName, isFetching: appNameIsFetching } = useAppNameQuery()
+
+const isLoading = computed(() => configIsFetching.value || appNameIsFetching.value)
 
 const input = ref('')
 const response = ref('')
@@ -13,19 +19,40 @@ async function onClick() {
 
 <template>
   <div class="flex flex-col items-center p-4 gap-4">
-    <h1 className="text-3xl font-bold underline text-blue-500">
-      Hello world!
-    </h1>
-
-    <div class="flex items-center gap-1">
-      <InputText v-model="input" name="username" type="text" placeholder="Enter your name" @keyup.enter="onClick" />
-      <Button label="Submit" @click="onClick" />
+    <div v-if="isLoading" class="flex flex-col justify-center items-center">
+      <ProgressSpinner />
+      <span class="text-blue-500">Loading...</span>
     </div>
 
-    <div class="flex flex-col items-center p-4">
-      <p class="text-lg font-semibold text-green-500">
+    <div v-else class="flex flex-col items-center p-4 gap-4">
+      <h1 className="text-3xl font-bold underline text-blue-500">
+        Hello world!
+      </h1>
+
+      <h2 className="text-xl font-semibold text-gray-500">
+        {{ appName }}
+      </h2>
+
+      <div class="flex items-center gap-1">
+        <InputText v-model="input" name="username" type="text" placeholder="Enter your name" @keyup.enter="onClick" />
+        <Button label="Submit" @click="onClick" />
+      </div>
+
+      <div class="flex flex-col items-center p-4">
+        <p class="text-lg font-semibold text-green-500">
+          {{ response }}
+        </p>
+      </div>
+      <div v-if="response" class="p-2 border border-gray-300 rounded">
         {{ response }}
-      </p>
+      </div>
+
+      <div class="mt-4">
+        <h2 class="text-lg font-bold text-blue-500">
+          Config
+        </h2>
+        <pre class="bg-gray-100 p-4 rounded-lg shadow-md w-full overflow-auto">{{ config }}</pre>
+      </div>
     </div>
   </div>
 </template>
--- frontend/src/views/Index.vue
+++ frontend/src/views/Index.vue
@@ -3,7 +3,7 @@ import Test from '@/components/Test.vue'
 </script>
 
 <template>
-  <main>
+  <main class="flex flex-col items-center justify-center">
     <Test />
   </main>
 </template>
