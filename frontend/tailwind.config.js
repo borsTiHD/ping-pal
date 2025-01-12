4700af439a9d5e494ee8105e545c6301940f9c4f
borsTiHD <bastian.j@gmx.net>
2025-01-12 17:29:29 +0100

    feat: update shadcn configuration

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -1,11 +1,10 @@
-import animate from 'tailwindcss-animate'
+const animate = require('tailwindcss-animate')
 
 /** @type {import('tailwindcss').Config} */
 export default {
   darkMode: ['selector', '[data-mode="dark"]'],
   safelist: ['dark'],
   prefix: '',
-
   content: [
     './index.html',
     './src/**/*.{vue,js,ts,jsx,tsx}',
@@ -15,7 +14,6 @@ export default {
     // './app/**/*.{ts,tsx,vue}',
     // './src/**/*.{ts,tsx,vue}',
   ],
-
   theme: {
     container: {
       center: true,
@@ -25,47 +23,6 @@ export default {
       },
     },
     extend: {
-      colors: {
-        border: 'hsl(var(--border))',
-        input: 'hsl(var(--input))',
-        ring: 'hsl(var(--ring))',
-        background: 'hsl(var(--background))',
-        foreground: 'hsl(var(--foreground))',
-        primary: {
-          DEFAULT: 'hsl(var(--primary))',
-          foreground: 'hsl(var(--primary-foreground))',
-        },
-        secondary: {
-          DEFAULT: 'hsl(var(--secondary))',
-          foreground: 'hsl(var(--secondary-foreground))',
-        },
-        destructive: {
-          DEFAULT: 'hsl(var(--destructive))',
-          foreground: 'hsl(var(--destructive-foreground))',
-        },
-        muted: {
-          DEFAULT: 'hsl(var(--muted))',
-          foreground: 'hsl(var(--muted-foreground))',
-        },
-        accent: {
-          DEFAULT: 'hsl(var(--accent))',
-          foreground: 'hsl(var(--accent-foreground))',
-        },
-        popover: {
-          DEFAULT: 'hsl(var(--popover))',
-          foreground: 'hsl(var(--popover-foreground))',
-        },
-        card: {
-          DEFAULT: 'hsl(var(--card))',
-          foreground: 'hsl(var(--card-foreground))',
-        },
-      },
-      borderRadius: {
-        xl: 'calc(var(--radius) + 4px)',
-        lg: 'var(--radius)',
-        md: 'calc(var(--radius) - 2px)',
-        sm: 'calc(var(--radius) - 4px)',
-      },
       keyframes: {
         'accordion-down': {
           from: { height: 0 },
