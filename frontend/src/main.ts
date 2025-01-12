e5743d1a09de98a7af39d12f53f0ff7ff897ee85
borsTiHD <bastian.j@gmx.net>
2025-01-12 15:19:49 +0100

    feat: integrate Shadcn

--- frontend/src/main.ts
+++ frontend/src/main.ts
@@ -6,7 +6,8 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
 
-import './assets/styles/tailwind.css'
+import './assets/styles/font.css'
+import './assets/styles/shadcn.css'
 import './assets/styles/main.css'
 
 const app = createApp(App)
