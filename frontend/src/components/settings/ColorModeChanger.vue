249221179b0f9a1528c533097312fde03c8264e8
borsTiHD <bastian.j@gmx.net>
2025-01-19 21:56:26 +0100

    wip: adding a new settings layout

--- frontend/src/components/settings/ColorModeChanger.vue
+++ frontend/src/components/settings/ColorModeChanger.vue
@@ -5,8 +5,16 @@ const { colorModeModel, colorModes } = useDarkModeState()
 </script>
 
 <template>
-  <div class="flex gap-2 items-center">
-    <span>Toggle Color Mode:</span>
-    <SelectButton v-model="colorModeModel" :options="colorModes" option-label="label" option-value="value" :allow-empty="false" />
+  <!-- TODO: Make a generic component for settings out of it -->
+  <div class="py-6 sm:flex">
+    <dt class="font-medium flex items-center sm:w-64 sm:flex-none sm:pr-6">
+      Color Mode
+    </dt>
+    <dd class="mt-1 flex justify-between items-center gap-x-6 sm:mt-0 sm:flex-auto">
+      <span class="text-sm text-surface-500 dark:text-surface-400">
+        Change the color mode of the application
+      </span>
+      <SelectButton v-model="colorModeModel" :options="colorModes" option-label="label" option-value="value" :allow-empty="false" />
+    </dd>
   </div>
 </template>
--- frontend/src/components/settings/Settings.vue
+++ frontend/src/components/settings/Settings.vue
@@ -9,22 +9,55 @@ const { data: config, isFetching: configIsFetching } = useConfigQuery()
 
 <template>
   <Card>
-    <template #title>
-      Settings
-    </template>
     <template #content>
-      <div class="flex flex-col items-center p-4 gap-4">
-        <div v-if="configIsFetching" class="flex flex-col justify-center items-center">
-          <ProgressSpinner />
-          <span class="text-primary-500">Loading...</span>
-        </div>
+      <div v-if="configIsFetching" class="flex flex-col justify-center items-center">
+        <ProgressSpinner />
+        <span class="text-primary-500">Loading...</span>
+      </div>
+      <div v-else class="px-4 py-16 sm:px-6 lg:flex-auto lg:px-0 lg:py-20">
+        <div class="mx-auto max-w-2xl space-y-16 sm:space-y-20 lg:mx-0 lg:max-w-none">
+          <!-- App Section -->
+          <div class="flex flex-col">
+            <h2>App Settings</h2>
+            <p class="mt-1 text-sm/6 text-surface-500 dark:text-surface-400">
+              This information will be displayed publicly so be careful what you share.
+            </p>
 
-        <div v-else class="flex flex-col items-center p-4 gap-4">
-          <div class="flex flex-col gap-4">
-            <h2>Color Mode</h2>
-            <ColorModeChanger />
+            <dl class="mt-6 divide-y divide-surface-200 dark:divide-surface-500 border-t border-surface-200 dark:border-surface-500 text-sm/6">
+              <ColorModeChanger />
+              <div class="py-6 sm:flex">
+                <dt class="font-medium  sm:w-64 sm:flex-none sm:pr-6">
+                  Email address
+                </dt>
+                <dd class="mt-1 flex justify-between gap-x-6 sm:mt-0 sm:flex-auto">
+                  <div class="">
+                    tom.cook@example.com
+                  </div>
+                  <button type="button" class="font-semibold text-primary-600 dark:text-primary-600 hover:text-primary-500">
+                    Update
+                  </button>
+                </dd>
+              </div>
+              <div class="py-6 sm:flex">
+                <dt class="font-medium  sm:w-64 sm:flex-none sm:pr-6">
+                  Title
+                </dt>
+                <dd class="mt-1 flex justify-between gap-x-6 sm:mt-0 sm:flex-auto">
+                  <div class="">
+                    Human Resources Manager
+                  </div>
+                  <button type="button" class="font-semibold text-primary-600 dark:text-primary-600 hover:text-primary-500">
+                    Update
+                  </button>
+                </dd>
+              </div>
+            </dl>
           </div>
+        </div>
+      </div>
 
+      <div class="flex flex-col items-center p-4 gap-4">
+        <div class="flex flex-col items-center p-4 gap-4">
           <div class="flex flex-col gap-4">
             <h2>Window Size</h2>
             <WindowSize />
