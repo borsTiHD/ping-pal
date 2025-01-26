3ec25a8c9d617c6b6ddd749f1232d25c4b83883e
borsTiHD <bastian.j@gmx.net>
2025-01-26 11:13:07 +0100

    feat: create SettingItem component for reusable settings layout

--- frontend/src/components/settings/ColorModeChanger.vue
+++ frontend/src/components/settings/ColorModeChanger.vue
@@ -1,20 +1,15 @@
 <script setup lang="ts">
+import SettingItem from '@/components/settings/SettingItem.vue'
 import { useConfig } from '@/composables/config'
 
 const { colorMode, colorModes } = useConfig()
 </script>
 
 <template>
-  <!-- TODO: Make a generic component for settings out of it -->
-  <div class="py-6 sm:flex">
-    <dt class="font-medium flex items-center sm:w-64 sm:flex-none sm:pr-6">
-      Color Mode
-    </dt>
-    <dd class="mt-1 flex justify-between items-center gap-x-6 sm:mt-0 sm:flex-auto">
-      <span class="text-sm text-surface-500 dark:text-surface-400">
-        Change the color mode of the application
-      </span>
-      <SelectButton v-model="colorMode" :options="colorModes" option-label="label" option-value="value" :allow-empty="false" />
-    </dd>
-  </div>
+  <SettingItem
+    label="Color Mode"
+    description="Change the color mode of the application"
+  >
+    <SelectButton v-model="colorMode" :options="colorModes" option-label="label" option-value="value" :allow-empty="false" />
+  </SettingItem>
 </template>
---
+++ frontend/src/components/settings/SettingItem.vue
@@ -0,0 +1,20 @@
+<script setup lang="ts">
+import { defineProps } from 'vue'
+
+interface Props { label: string, description: string }
+defineProps<Props>()
+</script>
+
+<template>
+  <div class="py-6 sm:flex">
+    <dt class="font-medium flex items-center sm:w-64 sm:flex-none sm:pr-6">
+      {{ label }}
+    </dt>
+    <dd class="mt-1 flex justify-between items-center gap-x-6 sm:mt-0 sm:flex-auto">
+      <span class="text-sm text-surface-500 dark:text-surface-400">
+        {{ description }}
+      </span>
+      <slot />
+    </dd>
+  </div>
+</template>
