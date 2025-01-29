729c4ecd9cf0575a6ec99642ce7341148be00f0e
borsTiHD <bastian.j@gmx.net>
2025-01-29 19:39:44 +0100

    feat: enhance ManageHosts component by integrating loading state into DataTable for improved user experience

--- frontend/src/components/hosts/ManageHosts.vue
+++ frontend/src/components/hosts/ManageHosts.vue
@@ -5,13 +5,8 @@ const { data: hostData, isFetching } = useHostsQuery()
 </script>
 
 <template>
-  <div v-if="isFetching" class="flex flex-col justify-center items-center">
-    <ProgressSpinner />
-    <span class="text-primary-500">Loading...</span>
-  </div>
-
-  <div v-else class="flex flex-col items-center p-4 border border-gray-300 rounded-lg shadow-md w-full">
-    <DataTable :value="hostData" table-style="min-width: 50rem">
+  <div class="flex flex-col items-center p-4 border border-gray-300 rounded-lg shadow-md w-full">
+    <DataTable :value="hostData" class="w-full" :loading="isFetching">
       <Column field="id" header="Id" />
       <Column field="name" header="Name" />
       <Column field="address" header="Address" />
